16dd54a2aef74955e68c159596bad8840808df3d
Savant <62388409+Savant404@users.noreply.github.com>
2021-02-18 22:19:58 +0200

    Add files via upload

---
+++ r_bot.py
@@ -0,0 +1,48 @@
+import time
+import os 
+import requests
+import praw
+
+
+start_time = time.time()
+
+
+# create reddit object instance
+reddit = praw.Reddit(client_id="B1Exp1wucyW2NQ",
+                     client_secret="7IAkDmmZM6CpvuzUaLWmHzzcC_YgeA",
+                     password="Honda_1995",
+                     user_agent="MGrab by u/Jelly_Man420",
+                     username="Jelly_Man420")
+
+# List of Subreddits to scrape...
+subreddits = [# Add sub-reddits to scrape... Example:
+              # "test",
+              # "test2",
+              # "etc",
+]
+
+cur_dir = os.getcwd()
+
+#need to add exeption handling...
+# turn bot into funtion... call funtion and add list as parameters....
+for i in subreddits:
+    print(i)
+    current_sub = reddit.subreddit(i)
+    print(current_sub.title)
+            # can change current_sub.hot to {.new or .top} 
+    for subs in current_sub.hot(limit=1): #limit can be changed to 1 - 60)
+        x = subs.title
+        print(x)
+        y = requests.get(subs.url)
+        z = subs.url
+        print(z)
+        with open(cur_dir +x+ ".jpg", 'wb') as f:
+            f.write(y.content)
+            f.close()
+            print("Downloaded Images: "+x+ ".......")
+
+
+end_time = time.time()
+
+
+print("Script took " + str(end_time - start_time) + " to run")
